0812c58dc4568ca7b8f78230589707422ef72ad3
arvindrajcs <arvindrajcs@gmail.com>
2025-01-16 11:17:28 +0530

    api

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,10 +1,16 @@
 
 
 export const API_HOST = 'http://103.133.215.182:8085';
+// export const API_HOST = 'http://localhost:1120';
+
 export const GET_HOTEL_TYPE = `/masterController/getAllHotelType`;
 export const SET_HOTEL_TYPE = `/masterController/createHotelType`;
 export const UPDATE_HOTEL_TYPE=`/masterController/updateHotelType`;
 export const AMENITIES='/masterController/hotel-amenities';
+export const TAGCATEGORY='/masterController/tagCategory';
+export const INCLUSION='/masterController/inclusion';
+
 export const LOGIN='/authController/login';
 
 export const GET_HOTEL_DETAILS = `/masterController/hotels`;
+
